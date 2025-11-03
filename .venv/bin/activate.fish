15c865890e82f51a1621ee603e0165b38886ead6
Noah Tilleul <noah.tilleul@etu.unilim.fr>
2025-11-03 14:53:51 +0100

    Tentative de douiller venv

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/fabio/SAE/GlossAI/.venv
+set -gx VIRTUAL_ENV ./.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
