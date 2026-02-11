6cd0d80399770681910403361689132f7626c34b
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2026-02-11 13:51:10 +0100

    feat: split names in parser

--- shell.nix
+++ shell.nix
@@ -9,5 +9,13 @@ pkgs.mkShell {
     libsoup_3
     webkitgtk_4_1
     openssl
+
+    gsettings-desktop-schemas
+    kdePackages.xdg-desktop-portal-kde
+    gvfs
   ];
+  shellHook = ''
+    export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
+    export GIO_EXTRA_MODULES="${pkgs.gvfs}/lib/gio/modules"
+  '';
 }
